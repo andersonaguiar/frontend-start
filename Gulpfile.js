097a98c81ad9495543d623b8bdb89f739a6dd0fd
Anderson Aguiar <revsoft10@gmail.com>
2014-08-10 11:49:55 -0300

    cria o Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,20 @@
+// Gulpfile.js
+// Require the needed packages
+var gulp = require('gulp');
+var nib = require('nib');
+
+function loadModule(module) {
+    global[module.replace(/^gulp-/, '')] = require(module);
+}
+
+require('matchdep').filterDev('gulp*').forEach(loadModule);
+
+gulp.task('nib', function(){
+    gulp.src('app/assets/stylus/**/*.styl')
+        .pipe(stylus({ use: nib(), compress: true }))
+        .pipe(gulp.dest('app/assets/css'));
+});
+
+gulp.task('w', function () {
+    gulp.watch(['app/assets/stylus/**/*.styl'], ['nib']);
+});
